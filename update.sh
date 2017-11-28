af2832ffd5e48d87802bdabb7bc2b2169ba0c319
Eike Trumann <git@eiketrumann.de>
2017-11-28 14:09:49 +0100

    Correct script path in update.sh

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ git fetch >> update.log
 commitahead=$(git rev-list HEAD...origin/master --count)
 if (($commitahead != 0))
 	then
-	./before_update.sh >> update.log
+	./before-update.sh >> update.log
 	git rebase >> update.log
-	./after_update.sh >> update.log
+	./after-update.sh >> update.log
 fi
